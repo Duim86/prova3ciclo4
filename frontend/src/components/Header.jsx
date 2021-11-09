3b1cfd3745cd5fabc4f65b39f6812c6beb622b6c
Duim86 <zanlorenzi@gmail.com>
2021-11-09 19:47:58 -0300

    config axios

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <div className="container mt-3">
+          <ul className="nav nav-tabs">
+              <li className="nav-item">
+                  <a aria-current="page" className="nav-link" href="/">Home</a>
+              </li>
+          </ul>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
+import Landing from './pages/Landing';
 
 function MainRoutes() {
   return (
   <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/contato/novo" element={<NovoContato />} />
+      {/* <Route path="/contato/novo" element={<NovoContato />} />
       <Route path={`/contato/editar/:id`} element={<EditarContato />} />
-      <Route path={`/contato/detalhes/:id`} element={<DetalhesContato />} />
+      <Route path={`/contato/detalhes/:id`} element={<DetalhesContato />} /> */}
   </Routes>
   );
 }
