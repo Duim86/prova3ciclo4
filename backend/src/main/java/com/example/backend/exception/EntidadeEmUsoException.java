5245399084f125e54d042182b456193a7b03e4f6
Duim86 <zanlorenzi@gmail.com>
2021-11-10 23:53:13 -0300

    fix tests

--- backend/src/main/java/com/example/backend/exception/EntidadeEmUsoException.java
+++ backend/src/main/java/com/example/backend/exception/EntidadeEmUsoException.java
@@ -3,7 +3,6 @@ package com.example.backend.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
 public class TelefoneConflictException extends RuntimeException{
   private static final long serialVersionUID = 1L;
 
